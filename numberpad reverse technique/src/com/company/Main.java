99175396ab7c8ddb137952fc9a71c811699b868e
Vishwanath Bondugula <40670894+vishwa5854@users.noreply.github.com>
2019-05-02 22:57:25 +0530

    no more bugs!!
    Atleast in my experience

--- numberpad reverse technique/src/com/company/Main.java
+++ numberpad reverse technique/src/com/company/Main.java
@@ -26,55 +26,50 @@ public class Main {
     private static void showSuggestions(Contacts[] contacts, String input) throws IOException {
         String[] finalArray = assignTheLengthValueToNewArray(input,contacts);
         for(int i=0;i<countNumberOfLinesInAFile();i++){
-            if(input.compareTo(contacts[i].numericalName) == 0){
-                System.out.println(contacts[i].actualName);
-            }
-            else if((finalArray[i].compareTo(input)) == 0){
-                System.out.println(contacts[i].actualName);
+            try {
+                if (input.compareTo(String.valueOf(contacts[i].numericalName)) == 0) {
+                    System.out.println(contacts[i].actualName);
+                } else if ((finalArray[i].compareTo(input)) == 0) {
+                    System.out.println(contacts[i].actualName);
+                }
+            }catch (NullPointerException ignored){
             }
         }
     }
 
     private static void assignNumericalValuesToContacts(Contacts[] contacts) {
-        for(int i=0;i<contacts.length;i++){
-            for(int j=0;j<contacts[i].actualName.length();j++){
-                if((contacts[i].actualName.charAt(j) == 'a') || (contacts[i].actualName.charAt(j) == 'b') || (contacts[i].actualName.charAt(j) == 'c')){
-                    contacts[i].numericalName += "2";
+        for (Contacts contact : contacts) {
+            for (int j = 0; j < contact.actualName.length(); j++) {
+                if(contact.actualName.charAt(j) == ' '){
+                    continue;
                 }
-                else if((contacts[i].actualName.charAt(j) == 'd') || (contacts[i].actualName.charAt(j) == 'e') || (contacts[i].actualName.charAt(j) == 'f')){
-                    contacts[i].numericalName += "3";
-                }
-                else if((contacts[i].actualName.charAt(j) == 'g') || (contacts[i].actualName.charAt(j) == 'h') || (contacts[i].actualName.charAt(j) == 'i')){
-                    contacts[i].numericalName += "4";
-                }
-                else if((contacts[i].actualName.charAt(j) == 'j') || (contacts[i].actualName.charAt(j) == 'k') || (contacts[i].actualName.charAt(j) == 'l')){
-                    contacts[i].numericalName += "5";
-                }
-                else if((contacts[i].actualName.charAt(j) == 'm') || (contacts[i].actualName.charAt(j) == 'n') || (contacts[i].actualName.charAt(j) == 'o')){
-                    contacts[i].numericalName += "6";
-                }
-                else if((contacts[i].actualName.charAt(j) == 'p') || (contacts[i].actualName.charAt(j) == 'q') || (contacts[i].actualName.charAt(j) == 'r') || (contacts[i].actualName.charAt(j) == 's')){
-                    contacts[i].numericalName += "7";
-                }
-                else if((contacts[i].actualName.charAt(j) == 't') || (contacts[i].actualName.charAt(j) == 'u') || (contacts[i].actualName.charAt(j) == 'v')){
-                    contacts[i].numericalName += "8";
-                }
-                else if((contacts[i].actualName.charAt(j) == 'w') || (contacts[i].actualName.charAt(j) == 'x') || (contacts[i].actualName.charAt(j) == 'y') || (contacts[i].actualName.charAt(j) == 'z')){
-                    contacts[i].numericalName += "9";
+                else if ((contact.actualName.charAt(j) == 'a') || (contact.actualName.charAt(j) == 'b') || (contact.actualName.charAt(j) == 'c')) {
+                    contact.numericalName.append("2");
+                } else if ((contact.actualName.charAt(j) == 'd') || (contact.actualName.charAt(j) == 'e') || (contact.actualName.charAt(j) == 'f')) {
+                    contact.numericalName.append("3");
+                } else if ((contact.actualName.charAt(j) == 'g') || (contact.actualName.charAt(j) == 'h') || (contact.actualName.charAt(j) == 'i')) {
+                    contact.numericalName.append("4");
+                } else if ((contact.actualName.charAt(j) == 'j') || (contact.actualName.charAt(j) == 'k') || (contact.actualName.charAt(j) == 'l')) {
+                    contact.numericalName.append("5");
+                } else if ((contact.actualName.charAt(j) == 'm') || (contact.actualName.charAt(j) == 'n') || (contact.actualName.charAt(j) == 'o')) {
+                    contact.numericalName.append("6");
+                } else if ((contact.actualName.charAt(j) == 'p') || (contact.actualName.charAt(j) == 'q') || (contact.actualName.charAt(j) == 'r') || (contact.actualName.charAt(j) == 's')) {
+                    contact.numericalName.append("7");
+                } else if ((contact.actualName.charAt(j) == 't') || (contact.actualName.charAt(j) == 'u') || (contact.actualName.charAt(j) == 'v')) {
+                    contact.numericalName.append("8");
+                } else if ((contact.actualName.charAt(j) == 'w') || (contact.actualName.charAt(j) == 'x') || (contact.actualName.charAt(j) == 'y') || (contact.actualName.charAt(j) == 'z')) {
+                    contact.numericalName.append("9");
                 }
             }
+            //System.out.println(contact.numericalName);
         }
     }
 
     private static String[] assignTheLengthValueToNewArray(String temp,Contacts[] contacts) throws IOException {
         String[] temp1 = new String[countNumberOfLinesInAFile()];
-        for (int i = 0; i < countNumberOfLinesInAFile(); i++) {
-            for (int j = 0; j < temp.length(); j++) {
-                if (j == 0) {
-                    temp1[i] = Character.toString(contacts[i].numericalName.charAt(j));
-                } else {
-                    temp1[i] += Character.toString(contacts[i].numericalName.charAt(j));
-                }
+        for (int i = 0; i < countNumberOfLinesInAFile();i++) {
+            if(contacts[i].numericalName.length() >= temp.length()) {
+                temp1[i] = contacts[i].numericalName.substring(0, temp.length());
             }
         }
         return temp1;
